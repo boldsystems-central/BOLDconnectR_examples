509a38a4ab9d0bc1705ab006dc22e21ddeac876c
Sameer Padhye <sameer.m.padhye@gmail.com>
2024-09-24 15:40:32 -0400

    update examples
    Code updated with new functionality

--- template_examples.R
+++ template_examples.R
@@ -19,33 +19,34 @@
 #c. retrieves specific columns
 
 #establish key
+devtools::install_github("https://github.com/boldsystems-central/BOLDconnectR")
+library(BOLDconnectR)
 
-library(boldconnectr)
-boldconnectr.key="dfdsfsfsdf"
+# Paste apikey in the function in single/double quotes
+bold.apikey()
 
 #option 1
-bcdm_df<-bold.fetch(get_by = "dataset/project", identifiers = c("DS-IBOLR24"))
+bcdm_df<-bold.fetch(get_by = "dataset_codes", identifiers = "DS-IBOLR24")
 
-#option 2
-bcdm_df<-bold.fetch(get_by = "sampleids", identifiers = braconids$samplids)
+#option 2 (using test.data sampleids)
+bcdm_df<-bold.fetch(get_by = "sampleid", identifiers = test.data$sampleid)
 
-#option 3
-bcdm_df<-bold.fetch(get_by = "processids", identifiers = braconids$processids)
+#option 3 (using test.data processids)
+bcdm_df<-bold.fetch(get_by = "processid", identifiers = test.data$processid)
 
-#option 4
+#option 4 
+
 bcdm_df<-bold.fetch(get_by = "bin", identifiers = braconids$bins)
 
-#operating mode a: load
-bcdm_df <- fetch.test.data<-bold.fetch(get_by = "dataset/project", identifiers = c("DS-IBOLR24"))
 
-#operating mode b: pass-thru - default is TSV and then it is overriden by what can inferred from the filename.  It would be nice if JSONL was supported but not critical or important.
-bcdm_subset_df<-bold.fetch(get_by = "dataset/project", identifiers = c("DS-IBOLR24"), export="~/braconid_export.", na.rm = TRUE)
+#operating mode b: pass-thru - default is TSV.  It would be nice if JSONL was supported but not critical or important. (will be saved in the working directory; working directory can be accessed by getwd())
 
+bcdm_subset_df<-bold.fetch(get_by = "dataset_codes", identifiers = "DS-IBOLR24", export="braconid_export", na.rm = TRUE)
 
 #column filters: throw an error if non-valid columns
-fetch.test.data<-bold.fetch(get_by = "dataset/project", identifiers = c("DS-IBOLR24"), export="~/braconid_export.", na.rm = TRUE, cols="csv of columns")
+fetch.test.data<-bold.fetch(get_by = "dataset_codes", identifiers = "DS-IBOLR24", export="braconid_export.", na.rm = TRUE, cols=c("bins"))
 
-#column presets: logic for this and above is: 1. generate a list of request fields; 2. confirm that all fields are BCDM; 3. apply filters while downloading
+#column presets: logic for this and above is: 1. generate a list of request fields; 2. confirm that all fields are BCDM; 3. apply filters while downloading. This has been used in the export function (presets)
 fetch.test.data<-bold.fetch(get_by = "dataset/project", identifiers = c("DS-IBOLR24"), col_presets="tax,geo", cols="processid,sampleid,collection_date")
 
 
