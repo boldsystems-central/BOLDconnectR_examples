317e6991987f17e2877c0d4efcd362bd9ff6d385
Sameer Padhye <sameer.m.padhye@gmail.com>
2024-11-11 10:48:30 -0500

    update check_packages

--- package_check_boldconnectr.R
+++ package_check_boldconnectr.R
@@ -1,6 +1,10 @@
 check_packages<-function()
 {
 
+cat(paste("The R version on your machine is: ",R.version$major,".",R.version$minor,sep=''))
+  
+cat("\n\nBOLDconnectR requires: R(>= 4.0.0)\n")  
+  
 installed_packages <- as.data.frame(installed.packages()[, c("Package", "Version")])
 
 dependencies <- "ape(>= 5.5), BAT(>= 2.0), data.table(>= 1.13), dplyr(>= 1.0.1), ggplot2(>= 3.3.2), httr(>= 1.4.2), jsonlite(>= 1.7),\nmaps(>= 3.3), methods, reshape2, rnaturalearth, sf(>= 0.9.4),\nskimr(>= 2.1.2), tidyr(>= 1.1.1), utils, vegan(>= 2.5.7)"
@@ -31,7 +35,7 @@ names(dependencies_available)[names(dependencies_available) == "v_greater_or_equ
 
 dependencies_available<-dependencies_available[,-3]
 
-cat("Displaying the dependent packages required by BOLDconnectR that are available with their respective versions and the minimum version required for running the package smoothly. The NA values in the min_version_required either mean that its a core package or the latest version is available on your machine if installed previously:\n\n")
+cat("\nDisplaying the dependent packages required by BOLDconnectR that are available on your machine with their respective versions and the minimum version required for running the package smoothly. The NA values in the min_version_required either mean that its a core package or the latest version is available on your machine if installed previously:\n\n")
 
 print(dependencies_available)
 
@@ -48,3 +52,4 @@ cat("\nDisplaying the suggested packages required by the bold.analyze.align and 
 print(suggestions_available)
 
 }
+check_packages()
